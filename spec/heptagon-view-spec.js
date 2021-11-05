d7114bc53fdb0273391912461deab5e04b3220a7
Théo Dupuy <teho.dupuy@gmail.com>
2021-11-05 12:03:20 +0100

    init commit

---
+++ spec/heptagon-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HeptagonView from '../lib/heptagon-view';
+
+describe('HeptagonView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
